e7c52776d88138c1ab9e52c541ebbb7c23e60236
rmchndran <164048641+rmchndran@users.noreply.github.com>
2024-05-02 12:10:45 -0500

    Updated TA Week 7 (Presentation Week)

--- Digital Chalkboard & Technical Assignments/Week 7/ochretest.js
+++ Digital Chalkboard & Technical Assignments/Week 7/ochretest.js
@@ -1,43 +1,63 @@
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    const fetch_id = document.body.getAttribute('data-uuid');
+    const fetch_id = document.body.getAttribute('data-uuid'); //Retrieves the uuid once the HTML has loaded completely.
     const ochre_url = 'https://ochre.lib.uchicago.edu/ochre?uuid=';
-    var display = document.getElementById('called-container');
-    let output = '';
+    var display = document.getElementById('called-container'); //Stores the intended 'display' element as a variable.
+    var table = null; //This is a flag to prevent the duplication of inner display elements.
 
 
-    function getXml(uuid) {
-        return fetch(ochre_url + uuid, {redirect:'follow'})
+    function getXml(uuid) { 
+        return fetch(ochre_url + uuid, {redirect:'follow'}) //Initiates the network request. 
         .then(output => {
-            if(!output.ok) throw new Error('Broken Link!');
-            return output.text();
+            if(!output.ok) throw new Error(`Request Invalid ${error}`); // Indicates if the request was not '200' HTTP.
+            return output.text(); //Extracts text content (XML strings).
         })
         .then(text_output => {
             const parser = new DOMParser();
-            return parser.parseFromString(text_output,'text/xml');
+            return parser.parseFromString(text_output,'text/xml'); //Returns XML document.
 
         });
     }
 
     function xml(data) {
-        const property = data.querySelectorAll('property');
-        let add_uuid = '';
+        const property = data.querySelectorAll('property'); //Extracts all 'property' tags from new XML Doc.
+        let add_uuid = ''; //
+        
+        if(!table) { //Verifies that a table hasn't been created yet.
+            table = document.createElement('table');
+            table.setAttribute('class','table table-hover');
+            tbody = document.createElement('tbody');
+            table.appendChild(tbody);
+            display.appendChild(table);
+        }
 
-        property.forEach(p => {
-            const string = p.querySelector('string');
-            const value = p.querySelector('value');
+        property.forEach(p => { //This iterates over each property tag.
+            const string = p.querySelector('string'); //Stores string tags.
+            const value = p.querySelector('value'); //Stores value tags.
 
-            if(string && value) {
-                output += `<strong>${string.textContent}</strong> | ${value.textContent} | <br><br>`
-                if(string.textContent == 'Associated text') {
-                    const titleLocation = document.getElementById('api-fetch-title');
+            if(string && value) { //Verifies that no data is missing.
+                var row = document.createElement('tr');
+                var strCell = document.createElement('td');
+                var valCell = document.createElement('td');
+                strCell.innerHTML = `<strong>${string.textContent}</strong>`;
+                valCell.innerHTML = `<strong>${value.textContent}</strong>`;
+                row.appendChild(strCell);
+                row.appendChild(valCell);
+                tbody.appendChild(row);
+
+                if(string.textContent == 'Associated text') { 
+                    const titleLocation = document.getElementById('api-fetch-title'); 
                     titleLocation.innerHTML = `<u><strong>~ ${value.textContent} ~</strong></u><br><br>`;
-                    add_uuid = value.getAttribute('uuid');
+                    add_uuid = value.getAttribute('uuid'); //Stores nested uuid for second iteration.
                 }
             }
         });
 
-        display.innerHTML = output;
-        return add_uuid;
+        display.appendChild(table); //Appends new table to the HTML display.
+        return add_uuid; //Ensures nested uuid is accessible.
     }
 
     getXml(fetch_id)
@@ -55,3 +75,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+
+//Here the fetch is actually intiated.
+//Predefined functions called appropriately.
+//Passes nested uuid back through if needed.
+//Catches any error in the fetch process.
+//Displays the error in the HTML.
